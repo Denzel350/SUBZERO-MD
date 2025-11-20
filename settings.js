13a19f2f34751394d665c78cc1cd36ef2be2554f
Denzel350 <anthonyotieno350@gmail.com>
2025-11-20 21:56:48 +0300

    Update OWNER_NUMBER and SESSION_ID in settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // Ice~6Pk8Ud "*"
+  OWNER_NUMBER: "254793678623", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
